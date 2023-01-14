6eeec07b646fc72a5a223ebd6cc8ce4e9f56d3fe
Florent Pergoud <florentpergoud@gmail.com>
2023-01-14 17:50:25 +0100

    feat(typescript): set up typescript template

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import * as operations from './operations';
-import * as shapes from './shapes';
+import * as fs from 'fs';
+import { cylinder, difference, sphere } from './scad-ts';
 
-export * from './operations';
-export * from './shapes';
+const size = 50;
+const hole = size / 2;
+const cylinderHeight = size * 2.5;
 
-export default { ...shapes, ...operations };
+const output = difference(
+	sphere(size),
+	cylinder(cylinderHeight, hole).rotate([0, 0, 0]),
+	cylinder(cylinderHeight, hole).rotate([0, 90, 0]).debug(),
+	cylinder(cylinderHeight, hole).rotate([90, 0, 0])
+);
+
+fs.writeFileSync('./dist/output.scad', output.serialize({ $fn: 100 }));
